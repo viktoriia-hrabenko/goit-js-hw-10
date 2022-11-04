dbb37775e584d49799e5e5c1e7c30e4393c4f3c4
Viktoriia Hrabenko <vikagrabenko666@gmail.com>
2022-11-04 15:50:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+
+const formInput = document.querySelector('input');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+formInput.addEventListener(
+  'input',
+  debounce(onSearchedCountry, DEBOUNCE_DELAY)
+);
+
+
+function clearAtrributes() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function onSearchedCountry(event) {
+  const countryToFind = event.target.value.trim();
+  if (!countryToFind) {
+    clearAtrributes();
+    return;
+  }
+
+  fetchCountries(countryToFind)
+    .then(country => {
+      if (country.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearAtrributes();
+        return;
+      } else if (country.length === 1) {
+        clearAtrributes(countryList.innerHTML);
+        renderCountryInfo(country);
+      } else if (country.length >= 2 && country.length <= 10) {
+        clearAtrributes(countryInfo.innerHTML);
+        renderCountryList(country);
+      }
+    })
+
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      clearAtrributes();
+      return error;
+    });
+};
+
+
+
+function renderCountryList(country) {
+  const markup = country
+    .map(({ name, flags }) => {
+      return `<li><img src="${flags.svg}" alt="${name.official}" width="100" height="60">${name.official}</li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function renderCountryInfo(country) {
+  const markupInfo = country
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<h1><img src="${flags.svg}" alt="${
+        name.official
+      }" width="100" height="60">${name.official}</h1>
+      <p><span>Capital: </span>${capital}</p>
+      <p><span>Population:</span> ${population}</p>
+      <p><span>Languages:</span> ${Object.values(languages)}</p>`;
+    })
+    .join('');
+  countryInfo.innerHTML = markupInfo;
+}
